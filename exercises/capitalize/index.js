d65efdab18a71ec62accc091389e33a7cb7e7556
Bayat Turtbayev <turtbayev@gmail.com>
2019-06-18 22:47:58 +0600

    another capitalize solution

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -8,10 +8,23 @@
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
 function capitalize(str) {
+    let result = str[0].toUpperCase();
+    for (let i = 1; i < str.length; i++) {
+        if (str[i-1] === ' ') {
+            result += str[i].toUpperCase();
+        } else {
+            result += str[i];
+        }
+    }
+    return result;
+}
+
+/*
+function capitalize(str) {
     return str
         .split(' ')
         .map(word => word[0].toUpperCase() + word.slice(1))
         .join(' ');
-}
+}*/
 
 module.exports = capitalize;
